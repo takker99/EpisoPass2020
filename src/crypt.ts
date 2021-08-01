9e8606738e9274fbf0b01dbeb7c74beccbf83157
takker99 <37929109+takker99@users.noreply.github.com>
2021-08-02 06:10:57 +0900

    :construction: use std/hash to calculate MD5 hash

--- src/crypt.ts
+++ src/crypt.ts
@@ -9,7 +9,10 @@
 // Toshiyuki Masui @ Pitecan.com
 // Last Modified: 2019/12/27
 //
-import { MD5_hexhash } from "./md5.ts";
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="dom" />
+import { createHash } from "https://deno.land/std@0.103.0/hash/mod.ts";
 import { __range__ } from "./utils.ts";
 
 //  文字種ごとに置換を行なうためのテーブル
@@ -27,47 +30,24 @@ const hexcharset = [
   "0123456789abcdef",
 ];
 
-let charset = origcharset;
-
-const charkind = (c: string) => {
+const charkind = (c: string, charset: string[]) => {
   return __range__(0, charset.length, false).findIndex((i) =>
     charset[i].indexOf(c) >= 0
   );
 };
 
 // crypt_char(crypt_char(c,n),n) == c になるような文字置換関数
-const crypt_char = function (c: string, n: number) {
-  const kind = charkind(c);
+const crypt_char = function (
+  c: string,
+  n: number,
+  charset: string[] = origcharset,
+) {
+  const kind = charkind(c, charset);
   const chars = charset[kind];
   const cind = chars.indexOf(c);
   const len = chars.length;
   const ind = ((n - cind) + len) % len;
   return chars[ind];
-};
-
-//
-// UTF8文字列をバイト文字列(?)に変換
-// (MD5_hexhashがUTF8データをうまく扱えないため)
-//
-const utf2bytestr = (text: string) => {
-  let result = "";
-  if (text === null) return result;
-  __range__(0, text.length, false).forEach(function (i) {
-    const c = text.charCodeAt(i);
-    if (c <= 0x7f) {
-      return result += String.fromCharCode(c);
-    } else {
-      if (c <= 0x07ff) {
-        result += String.fromCharCode(((c >> 6) & 0x1F) | 0xC0);
-        return result += String.fromCharCode((c & 0x3F) | 0x80);
-      } else {
-        result += String.fromCharCode(((c >> 12) & 0x0F) | 0xE0);
-        result += String.fromCharCode(((c >> 6) & 0x3F) | 0x80);
-        return result += String.fromCharCode((c & 0x3F) | 0x80);
-      }
-    }
-  });
-  return result;
 };
 
 //
@@ -77,23 +57,18 @@ const utf2bytestr = (text: string) => {
 export const crypt = (seed: string, secret_string: string) => {
   // ハッシュ値ぽいときHex文字だけ使うことにする。ちょっと心配だが...
   // Hex文字が32文字以上で、数字と英字が入ってればまぁハッシュ値と思って良いのではないか...
-  if (
-    seed.match(/[0-9a-f]{32}/) && seed.match(/[a-f]/) && seed.match(/[0-9]/)
-  ) {
-    charset = hexcharset;
-  } else {
-    charset = origcharset;
-  }
+  const useHex = seed.match(/[0-9a-f]{32}/) && seed.match(/[a-f]/) &&
+    seed.match(/[0-9]/);
 
   // secret_stringのMD5の32バイト値の一部を取り出して数値化し、
   // その値にもとづいて文字置換を行なう
-  const hash = MD5_hexhash(utf2bytestr(secret_string));
-  let res = "";
-  __range__(0, seed.length, false).forEach(function (i) {
+  const hash = createHash("md5");
+  hash.update(secret_string);
+  const hashString = hash.toString();
+  return __range__(0, seed.length, false).map((i) => {
     const j = i % 8;
-    const s = hash.substring(j * 4, (j * 4) + 4);
+    const s = hashString.substring(j * 4, (j * 4) + 4);
     const n = parseInt(s, 16);
-    return res += crypt_char(seed[i], n + i);
+    return crypt_char(seed[i], n + i, useHex ? hexcharset : origcharset);
   });
-  return res;
 };
